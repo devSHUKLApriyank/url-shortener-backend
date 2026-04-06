9e82d9f7419340bb4d7c073b233e0ef80cca831a
Priyank Shukla <umeshusha197@gmail.com>
2026-04-06 23:44:14 +0530

    Add frontend code

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,12 @@
+import { HomePage } from './pages/HomePage'
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
